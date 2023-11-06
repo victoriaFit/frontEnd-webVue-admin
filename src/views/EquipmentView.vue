6089dcaba9f05c77345b29c93ed540cfd11dc8e5
Augusto Balsanelli <github.891d8@passfwd.com>
2023-11-06 16:25:41 -0300

    Updated CSS

--- src/views/EquipmentView.vue
+++ src/views/EquipmentView.vue
@@ -9,7 +9,7 @@ const file = ref(null)
 const equipmentAtual = reactive({
   name: '',
   description: '',
-  cover_attachment_key: null, // Alterado de 'image' para 'cover_attachment_key'
+  cover_attachment_key: null,
 })
 
 function onFileChange(e) {
@@ -20,18 +20,17 @@ function onFileChange(e) {
 async function salvar() {
   if (file.value) {
     const imageResponse = await ImageService.uploadImage(file.value);
-    equipmentAtual.cover_attachment_key = imageResponse.attachment_key; // Ajuste aqui
+    equipmentAtual.cover_attachment_key = imageResponse.attachment_key;
   }
   await EquipmentApi.adicionarEquipment(equipmentAtual);
   Object.assign(equipmentAtual, {
     id: '',
     name: '',
     description: '',
-    cover_attachment_key: null // Alterado de 'image' para 'cover_attachment_key'
+    cover_attachment_key: null
   });
   coverUrl.value = '';
   file.value = null;
-  // Atualizar a lista de equipamentos
   equipments.value = await EquipmentApi.buscarTodosOsEquipments();
 }
 
@@ -41,19 +40,22 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
+  <div class="page-equipamentos">
     <h1>Equipamentos</h1>
-    <input type="file" @change="onFileChange">
-    <img v-if="coverUrl" :src="coverUrl" alt="Preview">
-    <input type="text" v-model="equipmentAtual.name" placeholder="Nome do equipamento">
-    <textarea v-model="equipmentAtual.description" placeholder="Descrição do equipamento"></textarea>
-    <button @click="salvar">Salvar Equipamento</button>
-    <div v-for="equipment in equipments" :key="equipment.id">
-      <!-- Alterado de 'equipment.image?.url' para 'equipment.cover?.url' -->
-      <img :src="equipment.cover?.url" :alt="equipment.name" />
-      <h3>{{ equipment.name }}</h3>
-      <p>{{ equipment.description }}</p>
-      <button @click="() => EquipmentApi.excluirEquipment(equipment.id)">Excluir</button>
+    <div class="equipment-form">
+      <input type="file" @change="onFileChange">
+      <img v-if="coverUrl" :src="coverUrl" alt="Preview" class="equipment-image">
+      <input type="text" v-model="equipmentAtual.name" placeholder="Nome do equipamento">
+      <textarea v-model="equipmentAtual.description" placeholder="Descrição do equipamento"></textarea>
+      <button @click="salvar">Salvar Equipamento</button>
+    </div>
+    <div class="equipment-list">
+      <div v-for="equipment in equipments" :key="equipment.id" class="equipment-card">
+        <img class="equipment-image" :src="equipment.cover?.url" :alt="equipment.name" />
+        <h3>{{ equipment.name }}</h3>
+        <p>{{ equipment.description }}</p>
+        <button @click="() => EquipmentApi.excluirEquipment(equipment.id)">Excluir</button>
+      </div>
     </div>
   </div>
 </template>
@@ -63,13 +65,17 @@ onMounted(async () => {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px;
+  padding: 40px;
 }
 
 .equipment-form {
   width: 100%;
   max-width: 600px;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .equipment-form input,
@@ -95,35 +101,49 @@ onMounted(async () => {
 }
 
 .equipment-list {
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 20px;
-  justify-content: center;
+  width: 100%;
+  max-width: 1200px;
+  margin-top: 20px;
 }
 
 .equipment-card {
   border: 1px solid #ddd;
-  border-radius: 4px;
-  padding: 15px;
-  width: calc(33.333% - 20px);
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  border-radius: 8px;
+  padding: 20px;
+  background: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  text-align: center;
 }
 
-.equipment-card img {
-  max-width: 100%;
-  height: auto;
+.equipment-card img.equipment-image {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 4px;
   margin-bottom: 15px;
 }
 
+.equipment-card h3 {
+  margin: 0;
+  color: #333;
+}
+
+.equipment-card p {
+  font-size: 0.9em;
+}
+
 @media (max-width: 768px) {
-  .equipment-card {
-    width: calc(50% - 20px);
+  .equipment-list {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
 }
 
 @media (max-width: 480px) {
-  .equipment-card {
-    width: 100%;
+  .equipment-list {
+    grid-template-columns: 1fr;
   }
 }
 </style>
